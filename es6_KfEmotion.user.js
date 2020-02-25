2abcd70d65b48ab897c2110a87ec5d252152651e
missttake <35645329+missttake@users.noreply.github.com>
2020-02-25 22:44:55 +0900

    Update es6_KfEmotion.user.js

--- es6_KfEmotion.user.js
+++ es6_KfEmotion.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        绯月表情增强插件
 // @namespace   https://greasyfork.org/users/5415
-// @version     5.1.3.1
+// @version     5.1.3.1.1
 // @author      eddie32
 // @description KF论坛专用的回复表情，插图扩展插件，在发帖时快速输入自定义表情和论坛BBCODE
-// @icon        https://blog.nekohand.moe/favicon.ico
+// @icon        https://mistake.tech/emote/favicon.ico
 // @homepage    https://github.com/liu599/KF-Emotion-UserScript
 // @include     http*://*2dkf.com/*
 // @include     http*://*9moe.com/*
@@ -21,7 +21,7 @@
 // ==/UserScript==
 'use strict';
 // 版本号
-const version = '5.1.3.1';
+const version = '5.1.3.1.1';
 // 网站是否为KfMobile
 const isKfMobile = typeof Info !== 'undefined' && typeof Info.imgPath !== 'undefined';
 
