11bf21434f784279adc4323d87992c4ab07a3994
missttake <35645329+missttake@users.noreply.github.com>
2020-02-25 22:37:22 +0900

    Create es6_KfEmotion.user.js

---
+++ es6_KfEmotion.user.js
@@ -0,0 +1,299 @@
+// ==UserScript==
+// @name        绯月表情增强插件
+// @namespace   https://greasyfork.org/users/5415
+// @version     5.1.3.1
+// @author      eddie32
+// @description KF论坛专用的回复表情，插图扩展插件，在发帖时快速输入自定义表情和论坛BBCODE
+// @icon        https://blog.nekohand.moe/favicon.ico
+// @homepage    https://github.com/liu599/KF-Emotion-UserScript
+// @include     http*://*2dkf.com/*
+// @include     http*://*9moe.com/*
+// @include     http*://*kfgal.com/*
+// @include     https://*miaola.info/*
+// @copyright   2014-2017, eddie32
+// @grant       none
+// @license     MIT
+// @run-at      document-end
+// @modifier1   喵拉布丁
+// @modifier2   mistakey
+// @modifier1-source https://raw.githubusercontent.com/miaolapd/KF_Online_Assistant/master/scripts/es6/KfEmotion.user.js
+// @modifier2-source https://raw.githubusercontent.com/missttake/KFOL_emote/master/es6_KfEmotion.user.js
+// ==/UserScript==
+'use strict';
+// 版本号
+const version = '5.1.3.1';
+// 网站是否为KfMobile
+const isKfMobile = typeof Info !== 'undefined' && typeof Info.imgPath !== 'undefined';
+
+// 灰企鹅
+const KfSmileList = [];
+const KfSmileCodeList = [];
+let kfImgPath = typeof imgpath !== 'undefined' ? imgpath : '';
+if (isKfMobile) kfImgPath = Info.imgPath;
+for (let i = 1; i < 49; i++) {
+    KfSmileList.push(`/${kfImgPath}/post/smile/em/em${(i) > 9 ? i : ('0' + i)}.gif`);
+    KfSmileCodeList.push(`[s:${i + 9}]`);
+}
+
+// AC娘表情
+const AcSmileList = [];
+for (let i = 1; i < 51; i++) {
+    AcSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds6/${i}.png`);
+}
+for (let i = 1; i < 40; i++) {
+    AcSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds5/${(i) > 9 ? i : ('0' + i)}.gif`);
+}
+
+// 常用表情
+const CommonSmileList = [];
+for (let i = 2; i < 64; i++) {
+    CommonSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds4/0xx${i}.png`);
+}
+
+// 阿卡林 from 摇曳百合
+const AkarinSmileList = [];
+for (let i = 1; i < 21; i++) {
+    AkarinSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds2/akari${i}.gif`);
+}
+for (let i = 1; i < 72; i++) {
+    AkarinSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds3/akari${i}.png`);
+}
+
+// B站和tora酱表情
+const BiliBiliSmileList = [];
+for (let i = 1; i < 17; i++) {
+    BiliBiliSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds/2233 (${i}).gif`);
+}
+for (let i = 1; i < 14; i++) {
+    BiliBiliSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds/${i}.png`);
+}
+for (let i = 0; i < 14; i++) {
+    BiliBiliSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds/bilibiliTV (${i}).png`);
+}
+for (let i = 1; i < 14; i++) {
+    BiliBiliSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/xds2/0${(i) > 9 ? i : ('0' + i)}.jpg`);
+}
+
+// lovelive表情（小）
+const LoveliveSmallSmileList = [];
+for (let i = 1; i < 41; i++) {
+    LoveliveSmallSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/lovelive/Lovelive2nd${i}.png`);
+}
+for (let i = 1; i < 41; i++) {
+    LoveliveSmallSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/lovelive/Lovelive${i}.png`);
+}
+
+// 少女歌剧
+const ShaoNvGeJuSmileList = [];
+for (let i = 1; i < 41; i++) {
+    ShaoNvGeJuSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/sticker (${i}).png`);
+}
+
+// バンドリ
+const BandoriSmileList = [];
+for (let i = 1; i < 41; i++) {
+    BandoriSmileList.push(`http://o6smnd6uw.bkt.clouddn.com/BGD/sticker (${i}).png`);
+}
+
+/**
+ * 表情菜单
+ */
+const MenuList = {
+    KfSmile: {datatype: 'imageLink', title: 'KF自带', addr: KfSmileList, ref: KfSmileCodeList},
+    Shortcut: {
+        datatype: 'plain',
+        title: '快捷',
+        addr: [
+            '[sell=100][/sell]', '[quote][/quote]', '[hide=100][/hide]', '[code][/code]', '[strike][/strike]', '[fly][/fly]',
+            '[color=#00FF00][/color]', '[b][/b]', '[u][/u]', '[i][/i]', '[hr]', '[backcolor=][/backcolor]', '[url=][/url]', '[img][/img]'
+        ],
+        ref: [
+            '出售贴sell=售价', '引用', '隐藏hide=神秘等级', '插入代码', '删除线', '跑马灯', '文字颜色', '粗体', '下划线', '斜体', '水平线', '背景色', '插入链接', '插入图片'
+        ]
+    },
+    Emoji: {
+        datatype: 'plain',
+        title: '颜文字',
+        addr: [
+            '(●・ 8 ・●)', '╰(๑◕ ▽ ◕๑)╯', '(ゝω・)', '〜♪♪', '(ﾟДﾟ≡ﾟДﾟ)', '(＾o＾)ﾉ', '(|||ﾟДﾟ)', '(`ε´ )', '(╬ﾟдﾟ)', '(|||ﾟдﾟ)', '(￣∇￣)',
+            '(￣3￣)', '(￣ｰ￣)', '(￣ . ￣)', '(￣︿￣)', '(￣︶￣)', '(*´ω`*)', '(・ω・)', '(⌒▽⌒)', '(￣▽￣）', '(=・ω・=)', '(｀・ω・´)',
+            '(〜￣△￣)〜', '(･∀･)', '(°∀°)ﾉ', '(￣3￣)', '╮(￣▽￣)╭', '( ´_ゝ｀)', 'のヮの', '(ﾉ؂< ๑）诶嘿☆～', '(<_<)', '(>_>)',
+            '(;¬_¬)', '(▔□▔)/', '(ﾟДﾟ≡ﾟдﾟ)!?', 'Σ(ﾟдﾟ;)', 'Σ( ￣□￣||)', '(´；ω；`)', '（/TДT)/', '(^・ω・^ )', '(｡･ω･｡)', '(●￣(ｴ)￣●)',
+            'ε=ε=(ノ≧∇≦)ノ', '(´･_･`)', '(-_-#)', '（￣へ￣）', '(￣ε(#￣) Σ', 'ヽ(`Д´)ﾉ', '(╯°口°)╯(┴—┴', '（#-_-)┯━┯', '_(:3」∠)_', '(笑)',
+            '(汗)', '(泣)', '(苦笑)', '(´・ω・`)', '(╯°□°）╯︵ ┻━┻', '(╯‵□′)╯︵┻━┻', '( ´ρ`)', '( ﾟωﾟ)', '(oﾟωﾟo)', '(　^ω^)', '(｡◕∀◕｡)',
+            '/( ◕‿‿◕ )\\', 'ε٩( º∀º )۶з', '(￣ε(#￣)☆╰╮(￣▽￣///)', '（●´3｀）~♪', '_(:з」∠)_', 'хорошо!', '＼(^o^)／', '(•̅灬•̅ )', '(ﾟДﾟ)',
+            'まったく、小学生は最高だぜ！！', 'ε=ε=ε=┏(゜ロ゜;)┛', '(；°ほ°)', '⎝≧⏝⏝≦⎠', 'ヽ(✿ﾟ▽ﾟ)ノ', '焔に舞い上がるスパークよ、邪悪な異性交際に、天罰を与え！',
+            '|•ω•`)'
+        ]
+    },
+    Acfun: {datatype: 'image', title: 'ACFUN', addr: AcSmileList},
+    Common: {datatype: 'image', title: '常用', addr: CommonSmileList},
+    Akari: {datatype: 'image', title: 'Akari', addr: AkarinSmileList},
+    BiliBili: {datatype: 'image', title: 'BiliBili', addr: BiliBiliSmileList},
+    LoveLive: {datatype: 'image', title: 'LoveLive', addr: LoveliveSmallSmileList},
+    ShaoNvGeJu: {datatype: 'image', title: '少女歌剧', addr: ShaoNvGeJuSmileList},
+    Bandori: {datatype: 'image', title: 'バンドリ', addr: BandoriSmileList},
+};
+
+/**
+ * 添加BBCode
+ * @param textArea 文本框
+ * @param {string} code BBCode
+ * @param {string} selText 选择文本
+ */
+const addCode = function (textArea, code, selText = '') {
+    let startPos = !selText ? (code.indexOf('[img]') > -1 || code.indexOf(']') < 0 ? code.length : code.indexOf(']') + 1) : code.indexOf(selText);
+    if (typeof textArea.selectionStart !== 'undefined') {
+        let prePos = textArea.selectionStart;
+        textArea.value = textArea.value.substring(0, prePos) + code + textArea.value.substring(textArea.selectionEnd);
+        textArea.selectionStart = prePos + startPos;
+        textArea.selectionEnd = prePos + startPos + selText.length;
+    }
+    else {
+        textArea.value += code;
+    }
+};
+
+/**
+ * 显示放大的表情图片
+ * @param {jQuery} $img 表情图片对象
+ */
+const showZoomInImage = function ($img) {
+    if ($img.get(0).naturalWidth <= $img.height()) return;
+    let offset = $img.offset();
+    let $zoomIn = $(`<img class="kfe-zoom-in" src="${$img.attr('src')}" alt="[预览图片]">`).appendTo('body');
+    let windowWidth = $(window).width();
+    let zoomInWidth = $zoomIn.outerWidth();
+    let top = offset.top - $zoomIn.outerHeight() - 5;
+    let left = offset.left + $img.width() / 2 - zoomInWidth / 2;
+    if (left < 0) left = 0;
+    else if (left + zoomInWidth > windowWidth) left = windowWidth - zoomInWidth;
+    $zoomIn.css({top, left});
+};
+
+/**
+ * 获取表情面板的HTML代码
+ * @param {string} key 菜单关键字
+ * @returns {string} 表情面板内容
+ */
+const getSmilePanelHtml = function (key) {
+    let data = MenuList[key];
+    if (!data) return '';
+    let html = '';
+    for (let i = 0; i < data.addr.length; i++) {
+        if (data.datatype === 'image') {
+            html += `<img class="kfe-smile" src="${data.addr[i]}" alt="[表情]">`;
+        }
+        else if (data.datatype === 'imageLink') {
+            let ref = typeof data.ref !== 'undefined' && typeof data.ref[i] !== 'undefined' ? data.ref[i] : '';
+            html += `<img class="kfe-smile" data-code="${ref}" src="${data.addr[i]}" alt="[表情]">`;
+        }
+        else if (data.datatype === 'plain') {
+            let ref = typeof data.ref !== 'undefined' && typeof data.ref[i] !== 'undefined' ? data.ref[i] : data.addr[i];
+            html += `<a class="kfe-smile-text" data-code="${data.addr[i]}" href="#">${ref}</a>`;
+        }
+    }
+    return `<div class="kfe-smile-panel" data-key="${key}">${html}</div>`;
+};
+
+/**
+ * 获取子菜单的HTML代码
+ * @returns {string} 子菜单内容
+ */
+const getSubMenuHtml = function () {
+    let html = '';
+    $.each(MenuList, function (key, data) {
+        html += `<a class="kfe-sub-menu" data-key="${key}" href="#" title="${data.title}">${data.title}</a>`;
+    });
+    return html;
+};
+
+/**
+ * 创建容器
+ * @param textArea 文本框
+ */
+const createContainer = function (textArea) {
+    let $container = $(`
+<div class="kfe-container">
+  <div class="kfe-menu">
+    <span title="made by eddie32 version ${version}; modified by 喵拉布丁" style="cursor: pointer;"><b>囧⑨</b></span>
+    ${getSubMenuHtml()}
+    <span class="kfe-close-panel">[-]</span>
+  </div>
+</div>
+`).insertBefore($(textArea));
+    $container.on('click', '.kfe-sub-menu', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+        let key = $this.data('key');
+        if (!key) return;
+        $container.find('.kfe-sub-menu').removeClass('kfe-sub-menu-active');
+        $this.addClass('kfe-sub-menu-active');
+        $container.find('.kfe-smile-panel').hide();
+        let $panel = $container.find(`.kfe-smile-panel[data-key="${key}"]`);
+        if ($panel.length > 0) $panel.show();
+        else $(getSmilePanelHtml(key)).appendTo($container).show();
+    }).on('click', '.kfe-smile, .kfe-smile-text', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+        let code = $this.data('code');
+        if (!code) code = `[img]${$this.attr('src')}[/img]`;
+        addCode(textArea, code);
+        if (/(Mobile|MIDP)/i.test(navigator.userAgent)) textArea.blur();
+        else textArea.focus();
+    }).on('mouseenter', '.kfe-smile', function () {
+        $('.kfe-zoom-in').remove();
+        showZoomInImage($(this));
+    }).on('mouseleave', '.kfe-smile', function () {
+        $('.kfe-zoom-in').remove();
+    }).find('.kfe-close-panel').click(function () {
+        $container.find('.kfe-smile-panel').hide();
+    });
+};
+
+/**
+ * 添加CSS
+ */
+const appendCss = function () {
+    $('head').append(`
+<style>
+  .kfe-container { padding: 5px; vertical-align: middle; font: 12px/1.7em "sans-serif"; }
+  .kfe-menu { margin-bottom: 5px; }
+  .kfe-sub-menu { margin: 0 7px; text-decoration: none; border-bottom: 2px solid transparent; }
+  .kfe-sub-menu:hover, .kfe-sub-menu:focus { text-decoration: none; border-color: deeppink; }
+  a.kfe-sub-menu-active { color: black }
+  .kfe-smile-panel { display: none; height: 120px; padding: 5px 3px; overflow-y: auto; border-top: 1px solid #ddd; }
+  .kfe-smile-panel[data-key="Shortcut"] { height: auto; }
+  .kfe-smile { display: inline-block; max-width: 60px; max-height: 60px; cursor: pointer; }
+  .kfe-smile-text { display: inline-block; padding: 3px 5px; }
+  .kfe-smile-text:hover { color: #fff !important; background-color: #2b2b2b; text-decoration: none; }
+  .kfe-close-panel { cursor: pointer; }
+  .kfe-zoom-in {
+    position: absolute; max-width: 150px; max-height: 150px; background-color: #fcfcfc; border: 3px solid rgba(242, 242, 242, 0.6);
+    border-radius: 2px; box-shadow: 0 0 3px rgb(102, 102, 102);
+  }
+</style>
+`);
+    if (isKfMobile) {
+        $('head').append(`
+<style>
+  #readPage .kfe-container, #writeMessagePage .kfe-container { margin-top: -10px; }
+  .kfe-menu { white-space: nowrap; overflow-x: auto; }
+</style>
+`);
+    }
+};
+
+/**
+ * 初始化
+ */
+const init = function () {
+    let $textAreas = $('textarea');
+    if (!$textAreas.length) return;
+    appendCss();
+    $textAreas.each(function () {
+        createContainer(this);
+    });
+};
+
+init();
