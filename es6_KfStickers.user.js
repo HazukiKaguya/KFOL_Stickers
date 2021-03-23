8bb1e0a44c48aa5dd6a332a9aed2449c497578b8
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2021-03-23 16:51:43 +0900

    Update es6_KfStickers.user.js

--- es6_KfStickers.user.js
+++ es6_KfStickers.user.js
@@ -5,7 +5,7 @@
 // @author      HazukiKaguya
 // @description KF论坛专用的回复表情，插图扩展插件，在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://mistake.tech/emote/favicon.ico
-// @homepage    https://github.com/HazukiKaguya/KFOL_Stickers
+// @homepage    https://mistakey.top/KFStickers
 // @include     http*://*2dkf.com/*
 // @include     http*://*9moe.com/*
 // @include     https://*kforz.com/*
