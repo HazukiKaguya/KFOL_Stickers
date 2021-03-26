0ff1a8ca7c7d5d505b77549b8f958a8aeb4635e3
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2021-03-26 11:19:25 +0900

    Update KFSticker.user.js
    0.0.3 完全修复其在喵拉移动版的kf原生表情显示（更换为相对路径），update some code to es6

--- KFSticker.user.js
+++ KFSticker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        绯月表情增强插件*改*Dev
 // @namespace   https://github.com/HazukiKaguya/KFOL_Stickers/blob/master/KFSticker.user.js
-// @version     0.0.2
+// @version     0.0.3
 // @author      HazukiKaguya
 // @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://sticker.inari.site/favicon.ico
@@ -20,10 +20,15 @@
 // @updateURL   https://github.com/HazukiKaguya/KFOL_Stickers/raw/master/KfSticker.user.js
 // ==/UserScript==
 // 原作者eddie32, 本分支由HazukiKaguya基于最新5.2.1版本魔改
-//0.0.2 修复imgpath，使其kf原生企鹅表情支持喵拉手机站
-//0.0.1 稳定版功能“表情贴纸旧域名替换为新域名”移植
+/*
+本次更新
+0.0.3 完全修复其在喵拉移动版的kf原生表情显示（更换为相对路径），update some code to es6
+历史更新
+0.0.2 修复imgpath，使其kf原生企鹅表情支持喵拉手机站
+0.0.1 稳定版功能“表情贴纸旧域名替换为新域名”移植
+*/
 // 版本号
-const version = '0.0.2';
+const version = '0.0.3';
 // 网站是否为KfMobile,修复喵拉手机版kf自带表情显示
 const isKfMobile = typeof Info !== 'undefined' && typeof Info.imgPath !== 'undefined';
 let FixImgPath="/post/smile/em/em"
