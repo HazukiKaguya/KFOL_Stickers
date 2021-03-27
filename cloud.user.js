5922a099ccf2a415c01f7393cc7d87f62feac311
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2021-03-27 11:59:08 +0900

    Update cloud.user.js

--- cloud.user.js
+++ cloud.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name      绯月表情增强插件*改*Dev云端版
-// @namespace     https://github.com/HazukiKaguya/KFOL_Stickers
+// @name        绯月表情增强插件*改*Dev云端版
+// @namespace   https://github.com/HazukiKaguya/KFOL_Stickers/blob/master/cloud.user.js
 // @version     9.9.9
-// @author     HazukiKaguya
-// @description     KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
-// @icon      https://sticker.inari.site/favicon.ico
+// @author      HazukiKaguya
+// @description KF论坛专用的回复表情, 插图扩展插件, 在发帖时快速输入自定义表情和论坛BBCODE
+// @icon        https://sticker.inari.site/favicon.ico
 // @homepage    https://mistakey.top/KFStickers
 // @include     https://*miaola.info/*
 // @include     https://*ikfol.com/*
@@ -13,12 +13,12 @@
 // @include     https://*kfgal.com/*
 // @include     https://*kforz.com/*
 // @include     https://*kfmax.com/*
-// @copyright     2021 HazukiKaguya
+// @copyright   2021 HazukiKaguya
 // @require     https://sticker.inari.site/stickerDev.js
-// @grant     none
+// @grant       none
 // @license     MIT
-// @run-at    document-end
-// @updateURL   https://github.com/HazukiKaguya/KFOL_Stickers/raw/master/cloud.js
+// @run-at      document-end
+// @updateURL   https://github.com/HazukiKaguya/KFOL_Stickers/raw/master/cloud.user.js
 // ==/UserScript==
 // 原作者eddie32, 本分支由HazukiKaguya基于最新5.2.1版本魔改 @copyright   2014-2019, eddie32 https://greasyfork.org/users/5415 https://github.com/liu599/KF-Emotion-UserScript
 /*
