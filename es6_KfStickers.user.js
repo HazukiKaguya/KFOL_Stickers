6da569dc8f2f0d575b76e91c8ca98e83280030bc
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2021-05-17 11:45:28 +0900

    删去失效域名9moe.com
    删去失效域名9moe.com

--- es6_KfStickers.user.js
+++ es6_KfStickers.user.js
@@ -6,7 +6,6 @@
 // @description KF论坛专用的回复表情，插图扩展插件，在发帖时快速输入自定义表情和论坛BBCODE
 // @icon        https://sticker.inari.site/favicon.ico
 // @homepage    https://mistakey.top/KFStickers
-// @include     http*://*9moe.com/*
 // @include     https://*kforz.com/*
 // @include     https://*kfmax.com/*
 // @include     http*://*kfgal.com/*
